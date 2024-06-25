408dd9352654b9f3cf4ac84958fb08597ce84645
theHassenYounsi <thehassenyounsi@gmail.com>
2024-06-25 13:18:01 +0100

    setup notif

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,179 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, Button } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { useEffect, useRef, useState } from "react";
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
+
+async function sendPushNotification(expoPushToken: string) {
+	const message = {
+		to: expoPushToken,
+		sound: "default",
+		title: "Original Title",
+		body: "And here is the body!",
+		data: { someData: "goes here" },
+	};
+
+	await fetch("https://exp.host/--/api/v2/push/send", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Accept-encoding": "gzip, deflate",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(message),
+	});
+}
+
+function handleRegistrationError(errorMessage: string) {
+	alert(errorMessage);
+	throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+	if (Platform.OS === "android") {
+		Notifications.setNotificationChannelAsync("default", {
+			name: "default",
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: "#FF231F7C",
+		});
+	}
+
+	if (Device.isDevice) {
+		const { status: existingStatus } =
+			await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== "granted") {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== "granted") {
+			handleRegistrationError(
+				"Permission not granted to get push token for push notification!"
+			);
+			return;
+		}
+		const projectId =
+			Constants?.expoConfig?.extra?.eas?.projectId ??
+			Constants?.easConfig?.projectId;
+		if (!projectId) {
+			handleRegistrationError("Project ID not found");
+		}
+		try {
+			const pushTokenString = (
+				await Notifications.getExpoPushTokenAsync({
+					projectId,
+				})
+			).data;
+			console.log(pushTokenString);
+			return pushTokenString;
+		} catch (e: unknown) {
+			handleRegistrationError(`${e}`);
+		}
+	} else {
+		handleRegistrationError("Must use physical device for push notifications");
+	}
+}
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	const [expoPushToken, setExpoPushToken] = useState("");
+	const [notification, setNotification] = useState<
+		Notifications.Notification | undefined
+	>(undefined);
+	const notificationListener = useRef<Notifications.Subscription>();
+	const responseListener = useRef<Notifications.Subscription>();
+
+	useEffect(() => {
+		registerForPushNotificationsAsync()
+			.then((token) => setExpoPushToken(token ?? ""))
+			.catch((error: any) => setExpoPushToken(`${error}`));
+
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) => {
+				setNotification(notification);
+			});
+
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				console.log(response);
+			});
+
+		return () => {
+			notificationListener.current &&
+				Notifications.removeNotificationSubscription(
+					notificationListener.current
+				);
+			responseListener.current &&
+				Notifications.removeNotificationSubscription(responseListener.current);
+		};
+	}, []);
+	return (
+		<ParallaxScrollView
+			headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+			headerImage={
+				<Image
+					source={require("@/assets/images/partial-react-logo.png")}
+					style={styles.reactLogo}
+				/>
+			}
+		>
+			<View
+				style={{
+					flex: 1,
+					alignItems: "center",
+					justifyContent: "space-around",
+				}}
+			>
+				<Text>Your Expo push token: {expoPushToken}</Text>
+				<View style={{ alignItems: "center", justifyContent: "center" }}>
+					<Text>
+						Title: {notification && notification.request.content.title}{" "}
+					</Text>
+					<Text>Body: {notification && notification.request.content.body}</Text>
+					<Text>
+						Data:{" "}
+						{notification && JSON.stringify(notification.request.content.data)}
+					</Text>
+				</View>
+				<Button
+					title="Press to Send Notification"
+					onPress={async () => {
+						await sendPushNotification(expoPushToken);
+					}}
+				/>
+			</View>
+		</ParallaxScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+	titleContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 8,
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8,
+	},
+	reactLogo: {
+		height: 178,
+		width: 290,
+		bottom: 0,
+		left: 0,
+		position: "absolute",
+	},
 });
